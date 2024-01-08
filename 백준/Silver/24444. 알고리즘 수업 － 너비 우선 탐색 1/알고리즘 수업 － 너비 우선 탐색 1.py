a099b04abaff48c3c5df56e5b84ac397b25721e8
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-01-08 10:01:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 548 ms, Memory: 59428 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,32 +1,29 @@
 import sys
-sys.setrecursionlimit(10**6) #재귀 깊이 설정
 from collections import deque
 input=sys.stdin.readline
-n,m,r=map(int,input().split()) # 정점 간선 시작정점
-graph=[[]for _ in range(n+1)] #인접리스트 
+n,m,r=map(int,input().split())
+graph=[[]for _ in range(n+1)] 
 visited=[False]*(n+1)
 order_n=[0]*(n+1)
 for _ in range(m):
-    a,b=map(int,input().split())
+    a,b=map(int,input().split()) 
     graph[a].append(b)
     graph[b].append(a)
 for x in range(n+1):
     graph[x].sort()
-
-cnt=0
+cnt=0 # 몇 번째에 방문했는지
 def bfs(r):
-    global cnt
+    global cnt 
     queue=deque([r])
     visited[r]=True
-    cnt+=1
-    order_n[r]=cnt
+    cnt+=1 
+    order_n[r]=cnt 
     while queue:
         v=queue.popleft()
-        # print(v,end=' ')
         for i in graph[v]:
-            if not visited[i]:
+            if not visited[i]: 
                 queue.append(i)
-                visited[i]=True
+                visited[i]=True 
                 cnt+=1
                 order_n[i]=cnt
 bfs(r)
