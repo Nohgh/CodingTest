824890a2e02b47a3b0be907eab6f90d350c66986
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-01-27 16:55:01 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 68 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.py
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.py
@@ -1,12 +1,6 @@
 import sys
 input=sys.stdin.readline
 n=int(input())
-arr=list(map(int,input().split()))
-arr2=[]
-for x in arr:
-    if arr2.count(x)>0:
-        continue
-    else:
-        arr2.append(x)
-arr2.sort()
-print(*arr2)
+arr=list(set(list(map(int,input().split()))))
+arr.sort()
+print(*arr)
