1e42b1878ee90116c2fe9f7194bc7f46c145c986
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-03-05 19:40:59 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 44 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -0,0 +1,12 @@
+#피보나치 수열을 반복문을 활용해 해결
+import sys
+input=sys.stdin.readline
+n=int(input())
+d=[0]*100000
+d[1]=1
+d[2]=1
+cnt=0
+for i in range(3,n+1):
+    d[i]=d[i-1]+d[i-2]
+    cnt+=1
+print(d[n],cnt)
