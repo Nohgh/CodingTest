c07bef541c505ccbbc8105d1d619ef16534114e4
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2024-08-14 18:14:20 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,12 +1,9 @@
-import sys
-a,b=[input() for _ in range(2)]
-a=int(a)
-b_100=int(b[0])
-b_10=int(b[1])
-b_1=int(b[2])
-b=int(b)
-#3
-print(a*(b_1))
-print(a*(b_10))
-print(a*(b_100))
-print(a*b)
+a=int(input())
+b=(input())
+b3=int(b[0])
+b2=int(b[1])
+b1=int(b[2])
+print(a*b1)
+print(a*b2)
+print(a*b3)
+print(a*int(b))
